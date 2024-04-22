f5fe0db933df1a484273e61cdf7555b55ca0e978
Tori Mayhall <torimayhall@gmail.com>
2024-04-22 15:04:53 -0500

    Add imnobodysoyeah page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def index
   end
+
+  def imnobodysoyeah
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root 'pages#index'
+
+  get 'imnobodysoyeah', to: 'pages#imnobodysoyeah', as: 'imnobodysoyeah'
 end
